49ff9522f5b0b83a896406d7f18de2c701dcbe91
Jire <technology@live.com>
2016-01-21 17:42:34 -0500

    Moved offsets to their own file, moved `Offsets.kt` content to `Offset`

--- src/main/kotlin/org/abendigo/csgo/CSGO.kt
+++ src/main/kotlin/org/abendigo/csgo/CSGO.kt
@@ -64,32 +64,6 @@ val m_iClip2 by bcwNetVar()
 // DT_WeaponCSBase
 val m_iWeaponID by netVar("DT_WeaponCSBase", "m_fAccuracyPenalty", 0x2C)
 
-// client.dll offsets
-val m_dwRadarBase by offset(client, 1, 0, READ or SUBTRACT, 161, 0, 0, 0, 0, 139, 12, 176, 139, 1, 255, 80, 0,
-		70, 59, 53, 0, 0, 0, 0, 124, 234, 139, 13, 0, 0, 0, 0)
-val m_dwWeaponTable by offset(client, 1, 0, READ or SUBTRACT, 161, 0, 0, 0, 0, 15, 183, 201, 3, 201, 139, 68, 0, 12, 195)
-val m_dwWeaponTableIndex by offset(client, 3, 0, READ, 102, 139, 142, 0, 0, 0, 0, 232, 0, 0, 0, 0, 5, 0, 0, 0, 0, 80)
-val m_dwInput by offset(client, 1, 0, READ or SUBTRACT, 185, 0, 0, 0, 0, 255, 117, 8, 232, 0, 0, 0, 0, 139, 6)
-val m_dwGlowObject by offset(client, 1, 4, READ or SUBTRACT, 161, 0, 0, 0, 0, 168, 1, 117, 0, 15, 87, 192, 199, 5)
-val m_dwForceJump by offset(client, 2, 0, READ or SUBTRACT, 137, 21, 0, 0, 0, 0, 139, 21, 0, 0, 0, 0, 246, 194, 3, 116, 3, 131, 206, 8)
-val m_dwForceAttack by offset(client, 2, 0, READ or SUBTRACT, 137, 21, 0, 0, 0, 0, 139, 21, 0, 0, 0, 0, 246, 194, 3, 116, 3, 131, 206, 4)
-val m_dwViewMatrix by offset(client, 828, 176, READ or SUBTRACT, 129, 198, 0, 0, 0, 0, 136, 69, 146, 15, 182, 192)
-val m_dwEntityList by offset(client, 1, 0, READ or SUBTRACT, 187, 0, 0, 0, 0, 131, 255, 1, 15, 140, 0, 0, 0, 0, 59, 248)
-val m_dwLocalPlayer by offset(client, 1, 16, READ or SUBTRACT, 163, 0, 0, 0, 0, 199, 5, 0, 0, 0, 0, 0, 0, 0, 0, 232, 0, 0, 0, 0, 89, 195, 106, 0)
-
-// engine.dll offsets
-val m_dwGlobalVars by offset(engine, 18, 0, READ or SUBTRACT, 0x8B, 0x0D, 0x0, 0x0, 0x0, 0x0, 0x83, 0xC4, 0x04,
-		0x8B, 0x01, 0x68, 0x0, 0x0, 0x0, 0x0, 0xFF, 0x35)
-val m_dwClientState by offset(engine, 1, 0, READ or SUBTRACT, 161, 0, 0, 0, 0, 243, 15, 17, 128, 0, 0, 0, 0, 217, 70, 4, 217, 5, 0, 0, 0, 0)
-val m_dwInGame by offset(engine, 2, 0, READ, 131, 185, 0, 0, 0, 0, 6, 15, 148, 192, 195)
-val m_dwMaxPlayer by offset(engine, 7, 0, READ, 161, 0, 0, 0, 0, 139, 128, 0, 0, 0, 0, 195, 204, 204, 204, 204, 85, 139, 236, 139, 69, 8)
-val m_dwMapDirectory by offset(engine, 1, 0, READ, 5, 0, 0, 0, 0, 195, 204, 204, 204, 204, 204, 204, 204, 128, 61)
-val m_dwMapname by offset(engine, 1, 0, READ, 5, 0, 0, 0, 0, 195, 204, 204, 204, 204, 204, 204, 204, 161, 0, 0, 0, 0)
-val m_dwPlayerInfo by offset(engine, 2, 0, READ, 139, 136, 0, 0, 0, 0, 139, 1, 139, 64, 0, 255, 208, 139, 248)
-val m_dwViewAngles by offset(engine, 4, 0, READ, 243, 15, 17, 128, 0, 0, 0, 0, 217, 70, 4, 217, 5, 0, 0, 0, 0)
-val m_dwEnginePosition by offset(engine, 4, 0, READ or SUBTRACT, 243, 15, 17, 21, 0, 0, 0, 0, 243, 15, 17, 13,
-		0, 0, 0, 0, 243, 15, 17, 5, 0, 0, 0, 0, 243, 15, 17, 61, 0, 0, 0, 0)
-
 // objects
 
 val clientState = cached { ClientState(engine.get(m_dwClientState)) }
--- src/main/kotlin/org/abendigo/csgo/offset/Offset.kt
+++ src/main/kotlin/org/abendigo/csgo/offset/Offset.kt
@@ -1,7 +1,11 @@
 package org.abendigo.csgo.offset
 
 import org.jire.kotmem.Module
+import org.jire.kotmem.NativeBuffer
 import kotlin.reflect.KProperty
+
+const val READ = 1
+const val SUBTRACT = 2
 
 data class Offset(val module: Module, val patternOffset: Int, val addressOffset: Int,
                   val flags: Int, val values: ByteArray) {
@@ -28,4 +32,32 @@ data class Offset(val module: Module, val patternOffset: Int, val addressOffset:
 		throw IllegalStateException("${ise.message} of property ${property.name}")
 	}
 
+}
+
+fun offset(module: Module, patternOffset: Int, addressOffset: Int, flags: Int, values: ByteArray)
+		= Offset(module, patternOffset, addressOffset, flags, values)
+
+fun offset(module: Module, patternOffset: Int, addressOffset: Int, flags: Int, vararg values: Int)
+		= offset(module, patternOffset, addressOffset, flags, toByteArray(*values))
+
+fun offset(module: Module, patternOffset: Int, addressOffset: Int, flags: Int, value: Int)
+		= offset(module, patternOffset, addressOffset, flags, toByteArray(value))
+
+fun offset(module: Module, patternOffset: Int, addressOffset: Int, flags: Int, className: String)
+		= offset(module, patternOffset, addressOffset, flags, className.toByteArray())
+
+internal fun checkMask(data: NativeBuffer, offset: Int, pMask: ByteArray): Boolean {
+	for (i in pMask.indices)
+		if (pMask[i].toInt() != 0 && (pMask[i] != data.getByte((offset + i).toLong())))
+			return false
+	return true
+}
+
+private fun toByteArray(value: Int) = byteArrayOf(value.toByte(), (value shr 8).toByte(),
+		(value shr 16).toByte(), (value shr 24).toByte())
+
+private fun toByteArray(vararg value: Int): ByteArray {
+	val array = ByteArray(value.size)
+	for (i in value.indices) array[i] = value[i].toByte()
+	return array
 }
