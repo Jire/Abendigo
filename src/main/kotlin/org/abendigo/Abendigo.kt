240bbd030f068ad4262c63542f2173ffdbeb7dd0
Jire <technology@live.com>
2016-01-23 04:19:39 -0500

    Disabled trigger bot by default

--- src/main/kotlin/org/abendigo/Abendigo.kt
+++ src/main/kotlin/org/abendigo/Abendigo.kt
@@ -3,7 +3,8 @@
 package org.abendigo
 
 import org.abendigo.csgo.Client.entities
-import org.abendigo.plugin.csgo.*
+import org.abendigo.plugin.csgo.BunnyHopPlugin
+import org.abendigo.plugin.csgo.ESPPlugin
 import org.abendigo.plugin.enable
 import org.abendigo.plugin.every
 import java.lang.management.ManagementFactory
@@ -20,5 +21,5 @@ fun main(args: Array<String>) {
 	enable(ESPPlugin)
 	// enable(AimAssistPlugin)
 	// enable(ReducedFlashPlugin)
-	enable(TriggerBotPlugin)
+	// enable(TriggerBotPlugin)
 }
