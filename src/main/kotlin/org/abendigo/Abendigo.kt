c998c6e31e0b651f39935b17ed04084020ff6ce0
Jire <technology@live.com>
2016-02-21 23:54:52 -0500

    Disabled RCS by default, increased entity updating to every 2 seconds

--- src/main/kotlin/org/abendigo/Abendigo.kt
+++ src/main/kotlin/org/abendigo/Abendigo.kt
@@ -3,7 +3,8 @@
 package org.abendigo
 
 import org.abendigo.csgo.Client.entities
-import org.abendigo.plugin.csgo.*
+import org.abendigo.plugin.csgo.BunnyHopPlugin
+import org.abendigo.plugin.csgo.ESPPlugin
 import org.abendigo.plugin.enable
 import org.abendigo.plugin.every
 import java.lang.management.ManagementFactory
@@ -12,14 +13,14 @@ import java.util.concurrent.TimeUnit.SECONDS
 fun main(args: Array<String>) {
 	println("Process ${ManagementFactory.getRuntimeMXBean().name}")
 
-	every(3, SECONDS) { +entities }
+	every(2, SECONDS) { +entities }
 
 	// TODO make a proper plugin system
 	enable(BunnyHopPlugin)
 	// enable(RadarPlugin) // bSpotted is being checked by client, do not use
 	enable(ESPPlugin)
-	enable(AimAssistPlugin)
+	// enable(AimAssistPlugin)
 	// enable(ReducedFlashPlugin)
 	// enable(TriggerBotPlugin)
-	enable(RCSPlugin)
+	// enable(RCSPlugin)
 }
