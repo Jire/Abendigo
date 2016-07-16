dc0a060482f14acb0c264f4196436ec0beac6744
Thomas Nappo <technology@live.com>
2016-07-15 22:46:24 -0400

    Added fake lag plugin

---
+++ src/main/kotlin/org/abendigo/plugin/csgo/FakeLagPlugin.kt
@@ -0,0 +1,28 @@
+package org.abendigo.plugin.csgo
+
+import org.abendigo.DEBUG
+import org.abendigo.csgo.engineDLL
+import org.abendigo.csgo.offsets.m_bSendPacket
+import org.abendigo.plugin.sleep
+import org.abendigo.util.random
+
+object FakeLagPlugin : InGamePlugin("Fake Lag", duration = 1) {
+
+	private const val MIN_DURATION = 80
+	private const val MAX_DURATION = 310
+
+	override fun cycle() {
+		try {
+			val sendPacket: Boolean = engineDLL[m_bSendPacket]
+			engineDLL[m_bSendPacket] = !sendPacket
+			sleep(random(MIN_DURATION, MAX_DURATION))
+		} catch (t: Throwable) {
+			if (DEBUG) t.printStackTrace()
+		}
+	}
+
+	override fun disable() {
+		engineDLL[m_bSendPacket] = true
+	}
+
+}
