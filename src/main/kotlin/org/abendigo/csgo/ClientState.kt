9058fe946458436debb94fab8c9a425ede7154fd
Thomas Nappo <technology@live.com>
2016-07-15 22:51:25 -0400

    Added silent angle option

--- src/main/kotlin/org/abendigo/csgo/ClientState.kt
+++ src/main/kotlin/org/abendigo/csgo/ClientState.kt
@@ -1,7 +1,12 @@
 package org.abendigo.csgo
 
 import org.abendigo.Addressable
+import org.abendigo.DEBUG
+import org.abendigo.csgo.Client.clientDLL
+import org.abendigo.csgo.Engine.clientState
+import org.abendigo.csgo.offsets.m_bSendPacket
 import org.abendigo.csgo.offsets.m_dwInGame
+import org.abendigo.csgo.offsets.m_dwInput
 import org.abendigo.csgo.offsets.m_dwViewAngles
 
 class ClientState(override val address: Int) : Addressable {
@@ -16,6 +21,43 @@ class ClientState(override val address: Int) : Addressable {
 		csgo[address + m_dwViewAngles] = angle.x // pitch (up and down)
 		csgo[address + m_dwViewAngles + 4] = angle.y // yaw (side to side)
 		// csgo[address + m_dwViewAngles + 8] = angle.z // roll (twist) p.s. don't use because it can cause untrusted
+	}
+
+	private fun silentAngle(angle: Vector<Float>) {
+		try {
+			val weapon = +Me().weapon
+			if (!weapon.canFire()) return
+
+			val userCMDBase: Int = clientDLL[m_dwInput + 0xEC]
+			var desiredTick: Int = csgo[clientState(1024).address + 0x4CB8]
+
+			engineDLL[m_bSendPacket] = false
+
+			for (i in 0..8) {
+				desiredTick += 1
+				val userCMD = userCMDBase + ((desiredTick % 150) * 0x64) // future usercmd
+
+				var previousAngle = Vector(0F, 0F, 0F)
+
+
+				var tick = 0
+				while (tick != desiredTick) {
+					previousAngle = angle()
+					tick = csgo[userCMD + 0x4]
+				}
+
+				for (i in 0..20) { // make sure we hit the timing at one point or another lol
+					csgo[userCMD + 0xC] = angle.x
+					csgo[userCMD + 0xC + 4] = angle.y
+				}
+
+				angle(previousAngle)
+			}
+
+			engineDLL[m_bSendPacket] = true
+		} catch (t: Throwable) {
+			if (DEBUG) t.printStackTrace()
+		}
 	}
 
 }
