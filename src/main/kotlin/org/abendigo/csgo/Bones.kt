49dbf653dd40265b905c35cd8cc3326589614516
facehooker <sneakclan@hotmail.com>
2016-12-02 01:47:06 +0100

    Update Bones.kt
    The bone.id for HEAD has changed to 8.

--- src/main/kotlin/org/abendigo/csgo/Bones.kt
+++ src/main/kotlin/org/abendigo/csgo/Bones.kt
@@ -2,6 +2,6 @@ package org.abendigo.csgo
 
 enum class Bones(override val id: Int) : Bone {
 
-	HEAD(6), NECK(5), UPPER_CHEST(4), LOWER_CHEST(3), LEGS(2), FEET(1)
+	HEAD(8), NECK(5), UPPER_CHEST(4), LOWER_CHEST(3), LEGS(2), FEET(1)
 
 }
